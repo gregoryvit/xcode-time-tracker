6302c1adc4b7cf5e1fef23ef7802d24d381eada3
Gregory Berngardt <gregoryvit@gmail.com>
2019-08-01 22:28:30 +0300

    Added device info parameter

--- scripts/project_end.py
+++ scripts/project_end.py
@@ -4,6 +4,15 @@ import sys,time,os
 from os.path import expanduser
 import subprocess
 
+
+def get_device_info():
+    try:
+        items = subprocess.check_output(['sysctl', '-n', 'machdep.cpu.brand_string', 'hw.model']).decode('utf-8').split('\n')
+        info = "| ".join([i for i in items if i])
+    except:
+        info = "No model and CPU info"
+    return info
+
 milliseconds = int(round(time.time() * 1000))
 line = ""
 with open(expanduser("~/.timecheck/end_time"), 'w') as f: 
@@ -25,9 +34,12 @@ except:
 
 print "It took " + str(diff) + " seconds to [" + activity + "] for " + project_name
 
+device_info = get_device_info()
+
 with open(expanduser("~/.timecheck/results"), 'a') as f: 
 	result_string = ",".join([
 		git_user,
+		device_info,
 		workspace_name, 
 		project_name, 
 		str(start_time), 
