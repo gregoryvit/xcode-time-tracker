17bbcafa0702d28a8be4b6aecf7234369b8a36a3
Gregory Berngardt <gregoryvit@gmail.com>
2019-08-01 22:39:17 +0300

    Added jarvis sender script

--- scripts/sender.py
+++ scripts/sender.py
@@ -1,4 +1,21 @@
+import requests
+import json
+
+
 def send_results(results):
-    print('Override sender.py')
-
-
+    try:
+        response = requests.post(
+            url="https://jarvis.surfstudio.ru/api/v1/analytics/buildinfo",
+            params={
+                "authToken": "5f7b7b74-a77b-4f6a-8104-78a71eab8bad",
+            },
+            headers={
+                "Content-Type": "application/json",
+            },
+            data=json.dumps({
+                "info": results
+            })
+        )
+        return response.status_code
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
